6fa63cb45c8383546be396865cc9e43918f74f0e
Devgen <evgenij.dauenhauer@hotmail.com>
2017-05-29 13:07:35 +0200

    still issue with lib jsf

--- smeartheartmate/src/main/java/de/tum/view/MainController.java
+++ smeartheartmate/src/main/java/de/tum/view/MainController.java
@@ -1,5 +1,46 @@
 package de.tum.view;
 
-public class MainController {
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class MainController implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6786807257668283513L;
+	
+	private String name;
+	private String primeName;
+	private String richName;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrimeName() {
+		return primeName;
+	}
+
+	public void setPrimeName(String primeName) {
+		this.primeName = primeName;
+	}
+
+	public String getRichName() {
+		return richName;
+	}
+
+	public void setRichName(String richName) {
+		this.richName = richName;
+	}
+
 
 }
